2074179aaa57f02d84b1fd597a7d432c33c89ff2
camila.o.s <camila.o.s@usp.br>
2023-11-27 09:40:33 -0300

    Commit_inicial

--- .virtualenvs/djangodev/Scripts/activate.bat
+++ .virtualenvs/djangodev/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\const\projetofinal_si\poliplanner\.virtualenvs\djangodev
+set VIRTUAL_ENV=C:\Users\milao\poliplanner\.virtualenvs\djangodev
 
 if not defined PROMPT set PROMPT=$P$G
 
